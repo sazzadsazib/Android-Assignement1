6892194031e1691ff9c01af2175fc5d57c5d8972
Md. Sazzadul Islam <sazib66@gmail.com>
2022-03-03 05:06:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/assignement1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignement1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
